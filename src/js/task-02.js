ca0face46b9ba21d9ce66d60d60f1dbadf543a07
SvyrydovFedir <118449931+SvyrydovFedir@users.noreply.github.com>
2023-04-23 20:50:15 +0300

    Add files via upload

---
+++ src/js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listElement = document.createElement("li");
+  listElement.classList.add("item");
+  listElement.textContent = ingredient;
+
+  ingredientsList.appendChild(listElement);
+});
