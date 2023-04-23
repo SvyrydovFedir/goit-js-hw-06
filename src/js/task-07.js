4affe5f1ba0a1e8c7b9cd2f12985e0627ff02651
SvyrydovFedir <118449931+SvyrydovFedir@users.noreply.github.com>
2023-04-24 00:35:52 +0300

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const fontSizeControl = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-const fontSizeChange = () => {
-  text.style.fontSize = `${fontSizeControl.value}px`;
-};
-
-fontSizeControl.addEventListener("input", fontSizeChange);
